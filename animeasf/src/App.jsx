37af8b221caf7a61564229841fbd16fa201a803c
AndreJarl <104331025+AndreJarl@users.noreply.github.com>
2023-08-03 12:33:41 +0800

    first commit

---
+++ animeasf/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function App() {
+  return (
+    <h1 className="sm:bg-black text-white h-full md:bg-red-600 lg:bg-yellow-600 xl:bg-indigo-700">
+      Hello world!
+    </h1>
+  )
+}
