165f2a7e8d3509951a955a43f75a165860e69e42
AndreJarl <104331025+AndreJarl@users.noreply.github.com>
2023-08-04 09:31:11 +0800

    styled navbar in tailwind~

--- animeasf/src/components/Navbar.jsx
+++ animeasf/src/components/Navbar.jsx
@@ -21,46 +21,46 @@ function Navbar() {
 
   
   return (
-    <div className='bg-black fixed w-full lg:flex lg:items-center lg:justify-center text-white lg:h-24'>
-       <div className="lg:flex lg:items-center lg:justify-center lg:gap-20 ">   
+    <div className='flex flex-col content-center items-center bg-black pb-9 lg:pb-9 lg:flex-row lg:content-center lg:mx-auto lg:gap-4 lg:h-30 lg:my-auto md:flex md:flex-row md:mx-auto md:pb-0 '>
+       <div className=" flex flex-col content-center pt-8 mx-10 lg:flex-row lg:items-center lg:content-center lg:mx-auto lg:gap-10 lg:my-auto md:flex md:mx-auto">   
             
-                <div className="lg:flex lg:items-center lg:justify-center lg:gap-10 ">
+                <div className="flex content-center items-center gap-8 mb-8 lg:mb-0 lg:gap-10" >
                 <Link to="/"  style={{textDecoration: 'none', color: 'white'}}>
-                  <h1 className='lg:text-4xl font-bold '>ANIMEASF</h1></Link>
-                    <ul className='lg:flex lg:items-center lg:justify-center lg:gap-4  lg:font-semibold cursor-pointer'>
+                  <h1 className='text-3xl font-bold md:text-4xl lg:text-4xl'>ANIMEASF</h1></Link>
+                    <ul className='flex gap-6 font-medium'>
                       <Link to="/topairing" style={{textDecoration: 'none', color: 'white'}}>
-                        <li className='hover:text-gray-500'>Trending</li>
+                        <li className='hover:text-gray-600'>Trending</li>
                       </Link>
                       <Link to="/recentanimes" style={{textDecoration: 'none', color: 'white'}}>
-                        <li className='hover:text-gray-500'>Recent</li>
+                        <li className='hover:text-gray-600'>Recent</li>
                         </Link>
                     </ul>
                 </div>
 
-                <div className='lg:flex lg:items-center lg:border lg:border-white lg:h-10 lg:p-3 cursor-pointer lg:rounded-lg bg-black '> 
-                    <input className='lg:w-60 lg:h-5 border-0 bg-black' style={{ outline: 'none', color: 'white'}}
+                <div className='sm:hidden lg:flex lg:items-center border rounded-lg   '> 
+                    <input className='bg-black text-white w-80 py-2 px-2 rounded-lg' style={{ outline: 'none', color: 'white'}}
                      type="text" placeholder='search anime' value={animes}
                     onChange={(event)=> setAnime(event.target.value)}/>
                     {animes.trim() !== "" ? 
-                    ( <Link to = {`/search/${animes}`} onClick={Search} ><h2 className='lg:text-2xl '>
+                    ( <Link to = {`/search/${animes}`} onClick={Search} ><h2 className='lg:text-2xl px-1 cursor-pointer'>
                       <FcSearch /></h2></Link>)
-                     : (<h2 className='lg:text-2xl '><FcSearch /></h2>)}
+                     : (<h2 className='lg:text-2xl px-1 cursor-pointer'><FcSearch /></h2>)}
                 </div>
 
-                <div className='lg:flex lg:items-center lg:gap-5 lg:text-2xl '>
-                    <h2 className='cursor-pointer'><BsMoonFill/></h2> 
-                    <h2 className='cursor-pointer'><FaGithubAlt/></h2> 
-                    <h2 className='cursor-pointer'><FaUserCircle/></h2> 
+                <div className='sm:hidden lg:flex gap-6'>
+                    <h2 className='cursor-pointer text-2xl hover:text-gray-600'><BsMoonFill/></h2> 
+                    <h2 className='cursor-pointer  text-2xl hover:text-gray-600'><FaGithubAlt/></h2> 
+                    <h2 className='cursor-pointer  text-2xl hover:text-gray-600'><FaUserCircle/></h2> 
                 </div>
 
          </div>
 
-         <div className='mobileSearchContainer' style={{display: 'none'}}> 
-                    <input style={{ outline: 'none'}} type="text" placeholder='search anime' value={animes}
+         <div className=' flex content-center items-center gap-4 mx-10 lg:hidden '> 
+                    <input className='bg-white text-black w-60 h-8 rounded py-1 px-1 ' style={{ outline: 'none'}} type="text" placeholder='search anime' value={animes}
                     onChange={(event)=> setAnime(event.target.value)}/>
                     {animes.trim() !== "" ? 
-                    ( <Link to = {`/search/${animes}`} onClick={Search} ><h2><FcSearch /></h2></Link>)
-                     : (<h2><FcSearch /></h2>)}
+                    ( <Link to = {`/search/${animes}`} onClick={Search} ><h2 className='text-2xl bg-gray-500 px-2 py-1 rounded hover:bg-slate-800'><FcSearch /></h2></Link>)
+                     : (<h2 className='text-2xl bg-gray-500 px-2 py-1 rounded hover:bg-slate-800'><FcSearch /></h2>)}
                 </div>
     </div>
   )
